1699cf93778af76cae81e316db44547be58c5dc0
duru.hi <hwanggyuil@gmail.com>
2023-09-20 09:31:01 +0900

    feat. 이미지 확인을 위한 BindingAdapters 추가

---
+++ app/src/main/java/camp/nextstep/edu/github/ui/PhotoBindingAdapters.kt
@@ -0,0 +1,14 @@
+package camp.nextstep.edu.github.ui
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("showImage")
+fun bindShowImage(imageView: ImageView, url: String?) {
+    url?.let {
+        Glide.with(imageView.context)
+            .load(url)
+            .into(imageView)
+    }
+}

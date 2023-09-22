34760f6b65014f5c8e289a88cae0563ddd6052cd
duru.hi <hwanggyuil@gmail.com>
2023-09-22 11:53:44 +0900

    fix. Paging 리스너 수정(기존 ScrollOffset 방식 Deprecated)

--- app/src/main/java/camp/nextstep/edu/github/util/addOnPagingListener.kt
+++ app/src/main/java/camp/nextstep/edu/github/util/addOnPagingListener.kt
@@ -1,5 +1,6 @@
 package camp.nextstep.edu.github.util
 
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 fun RecyclerView.addOnPagingListener(
@@ -9,6 +10,15 @@ fun RecyclerView.addOnPagingListener(
     recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
         override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
             super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = (recyclerView.layoutManager as LinearLayoutManager)
+            val firstVisibleItemIndex = layoutManager.findFirstVisibleItemPosition()
+            if (firstVisibleItemIndex == (layoutManager.itemCount - NEXT_PAGE_TRIGGER)) {
+                arrivedBottom()
+                return
+            }
+
+            @Deprecated("Deprecated된 메서드 사용")
             if (scrollPercent(recyclerView) >= 100) {
                 arrivedBottom()
                 return
@@ -17,9 +27,13 @@ fun RecyclerView.addOnPagingListener(
     })
 }
 
+@Deprecated("대체: FirstVisibleItemPosition()\n이유: 10000개의 리스트를 볼경우 9000이후 부터 무한으로 호출",
+    ReplaceWith("(recyclerView.computeVerticalScrollOffset() * 1.0 / (recyclerView.computeVerticalScrollRange() - recyclerView.computeVerticalScrollExtent())) * 100.0")
+)
 fun scrollPercent(recyclerView: RecyclerView): Double {
     return (recyclerView.computeVerticalScrollOffset() * 1.0 / (recyclerView.computeVerticalScrollRange() - recyclerView.computeVerticalScrollExtent())) * 100.0
 }
 
 const val PAGE_SIZE = 20
 const val INIT_PAGE = 0
+private const val NEXT_PAGE_TRIGGER = PAGE_SIZE / 2
